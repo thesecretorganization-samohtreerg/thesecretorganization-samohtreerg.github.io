f9c6320438caac6f13a64e73a752c80de8fa6394
Micah <micah.rasmussen0@gmail.com>
2025-11-10 12:38:09 -0600

    Add message on destroy screen

--- org-state.js
+++ org-state.js
@@ -3,9 +3,16 @@
     const destroyed = localStorage.getItem('org_destroyed') === '1';
     if (!destroyed) return;
     document.open();
-    document.write('');
+    document.write('the organization has been destroyed');
     document.close();
     document.body.style.backgroundColor = '#000';
+    document.body.style.color = 'white';
+    document.body.style.fontFamily = 'ui-monospace, SFMono-Regular, Menlo, Consolas, "Liberation Mono", monospace';
+    document.body.style.height = '100%';
+    document.body.style.display = 'flex';
+    document.body.style.justifyContent = 'center';
+    document.body.style.alignItems = 'center';
+    document.body.style.margin = '0';
   } catch (e) {}
 })();
 
